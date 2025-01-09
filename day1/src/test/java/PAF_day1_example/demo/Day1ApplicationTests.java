99450bc4f335c9912374bf3b6271929c6fab4555
Lim Yu Aun <limyuaun3@gmail.com>
2025-01-09 11:43:45 +0800

    commit

---
+++ day1/src/test/java/PAF_day1_example/demo/Day1ApplicationTests.java
@@ -0,0 +1,13 @@
+package PAF_day1_example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
